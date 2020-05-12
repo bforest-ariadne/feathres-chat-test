4f2c68a1dcbb62070842e9be53e72772f02f5819
Ben Forest <ben@ben-forest.com>
2020-05-12 14:13:13 -0400

    add login page

--- public/app.js
+++ public/app.js
@@ -7,15 +7,20 @@ client.configure(feathers.authentication({
   storage: window.localStorage
 }));
 
-const login = async () => {
+const login = async (credentials) => {
   try {
-    return await client.reAuthenticate();
+    if (!credentials) {
+      await client.reAuthenticate();
+    } else {
+      await client.authenticate({
+        strategy: 'local',
+        ...credentials
+      });
+    }
+    // show chat messages
+    console.log('signup / login success! go to chat window...');
   } catch (error) {
-    return await client.authenticate({
-      strategy: 'local',
-      email: 'hello@feathersjs.com',
-      password: 'supersecret'
-    });
+    showLogin(error);
   }
 };
 
@@ -29,3 +34,71 @@ const main = async () => {
 
 console.log('app.js');
 main();
+
+const loginHTML = `
+<main class="login container">
+  <div class="row">
+    <div class="col-12 col-6-tablet push-3-tablet text-center heading">
+      <h1 class="font-100">Login or signup</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class = col-12 col-6-tablet push-3-tablet col-4-desktop push-4-desktop">
+      <form class="form">
+        <fieldset>
+          <input class="block" type="email" name="email" placeholder="email">
+        </fieldset>
+        <fieldset>
+          <input class="block" type="password" name="password" placeholder="password">
+        </fieldset>
+        <button type="button" id="login" class="button button-primary block signup">
+          Log in
+        </button>
+        <button type="button" id="signup" class="button button-primary block signup">
+          Sign up and login
+        </button>
+        <a class="button button-primary block" href="/oauth/github">
+          Login with Github
+        </a>
+      </form>
+    </div>
+  </div>
+</main>
+`;
+
+const chatHTML = `
+`;
+
+const showLogin = (error) => {
+  if (document.querySelectorAll('.login').length && error) {
+    document.querySelector('.heading').insertAdjacentHTML('beforeend', `<p>There was an error: ${error.message}</p>`);
+  } else {
+    document.getElementById('app').innerHTML = loginHTML;
+  }
+};
+
+showLogin();
+
+const getCredentials = () => {
+  const user = {
+    email: document.querySelector('[name="email"]').value,
+    password: document.querySelector('[name="password"]').value
+  };
+  return user;
+};
+
+const addEventListener = (selector, event, handler) => {
+  document.addEventListener(event, async ev => {
+    if (ev.target.closest(selector)) {
+      handler(ev);
+    }
+  });
+};
+
+addEventListener('#signup', 'click', async () => {
+  const credentials = getCredentials();
+  await client.service('users').create(credentials);
+
+  await login(credentials);
+
+});
